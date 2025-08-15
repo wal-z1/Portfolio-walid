db604b6dc3a19cc6869e7c22d639639dac25921f
Walid <ow_bouhenika@esi.dz>
2025-08-15 15:15:23 +0100

    muh ill finish this now

--- my-portfolio/src/components/Skills/Skills.jsx
+++ my-portfolio/src/components/Skills/Skills.jsx
@@ -5,44 +5,46 @@ import SkillCard from "./Skillcard.jsx";
 function Skills() {
 	return (
 		<>
-			<h1 className="bg-gradient-to-b from-slate-800 via-violet-500 to-zinc-700 bg-clip-text text-transparent">
-				test
-			</h1>
-			<div
-				className=" px-10 py-13 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8
-  bg-[#09090b]/20   border-t-2   border-purple-700/50  shadow-[0_-15px_30px_-10px_rgba(168,85,247,0.2)] 
-  bg-blend-overlay
-  bg-[url('')]">
-				<SkillCard
-					title={"hi"}
-					description={"hiii"}
-					imageUrl={"https://placehold.co/300"}
-				/>
-				<SkillCard
-					title={"hi"}
-					description={"hiii"}
-					imageUrl={"https://placehold.co/300"}
-				/>
-				<SkillCard
-					title={"hi"}
-					description={"hiii"}
-					imageUrl={"https://placehold.co/300"}
-				/>
-				<SkillCard
-					title={"hi"}
-					description={"hiii"}
-					imageUrl={"https://placehold.co/300"}
-				/>
-				<SkillCard
-					title={"hi"}
-					description={"hiii"}
-					imageUrl={"https://placehold.co/300"}
-				/>
-				<SkillCard
-					title={"hi"}
-					description={"hiii"}
-					imageUrl={"https://placehold.co/300"}
-				/>
+			<div>
+				<h1
+					className="text-center font-bold text-slate-100 mb-4 font-outfit leading-relaxed
+">
+						Skills
+				</h1>
+				<div
+					className=" px-10 py-13 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8
+  bg-[#09090b]/20   border-t-2   border-purple-700/50  shadow-[0_-15px_30px_-10px_rgba(168,85,247,0.2)] bg-[url('/Images/noise.svg')]">
+					<SkillCard
+						title={"hi"}
+						description={"hiii"}
+						imageUrl={"https://placehold.co/300"}
+					/>
+					<SkillCard
+						title={"hi"}
+						description={"hiii"}
+						imageUrl={"https://placehold.co/300"}
+					/>
+					<SkillCard
+						title={"hi"}
+						description={"hiii"}
+						imageUrl={"https://placehold.co/300"}
+					/>
+					<SkillCard
+						title={"hi"}
+						description={"hiii"}
+						imageUrl={"https://placehold.co/300"}
+					/>
+					<SkillCard
+						title={"hi"}
+						description={"hiii"}
+						imageUrl={"https://placehold.co/300"}
+					/>
+					<SkillCard
+						title={"hi"}
+						description={"hiii"}
+						imageUrl={"https://placehold.co/300"}
+					/>
+				</div>
 			</div>
 		</>
 	);
