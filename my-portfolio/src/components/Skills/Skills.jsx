779e748b4683916d8dd284a1eff59527bfceb4dc
Walid <ow_bouhenika@esi.dz>
2025-08-15 14:26:38 +0100

    adjusting the skill section

--- my-portfolio/src/components/Skills/Skills.jsx
+++ my-portfolio/src/components/Skills/Skills.jsx
@@ -4,38 +4,43 @@ import SkillCard from "./Skillcard.jsx";
 }
 function Skills() {
 	return (
-		<div className=" px-10 py-13 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8">
-			<SkillCard
-				title={"hi"}
-				description={"hiii"}
-				imageUrl={"https://placehold.co/300"}
-			/>
-			<SkillCard
-				title={"hi"}
-				description={"hiii"}
-				imageUrl={"https://placehold.co/300"}
-			/>
-			<SkillCard
-				title={"hi"}
-				description={"hiii"}
-				imageUrl={"https://placehold.co/300"}
-			/>
-			<SkillCard
-				title={"hi"}
-				description={"hiii"}
-				imageUrl={"https://placehold.co/300"}
-			/>
-			<SkillCard
-				title={"hi"}
-				description={"hiii"}
-				imageUrl={"https://placehold.co/300"}
-			/>
-			<SkillCard
-				title={"hi"}
-				description={"hiii"}
-				imageUrl={"https://placehold.co/300"}
-			/>
-		</div>
+		<>
+			<h1 className="bg-gradient-to-b from-slate-800 via-violet-500 to-zinc-700 bg-clip-text text-transparent">
+				test
+			</h1>
+			<div className=" px-10 py-13 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8">
+				<SkillCard
+					title={"hi"}
+					description={"hiii"}
+					imageUrl={"https://placehold.co/300"}
+				/>
+				<SkillCard
+					title={"hi"}
+					description={"hiii"}
+					imageUrl={"https://placehold.co/300"}
+				/>
+				<SkillCard
+					title={"hi"}
+					description={"hiii"}
+					imageUrl={"https://placehold.co/300"}
+				/>
+				<SkillCard
+					title={"hi"}
+					description={"hiii"}
+					imageUrl={"https://placehold.co/300"}
+				/>
+				<SkillCard
+					title={"hi"}
+					description={"hiii"}
+					imageUrl={"https://placehold.co/300"}
+				/>
+				<SkillCard
+					title={"hi"}
+					description={"hiii"}
+					imageUrl={"https://placehold.co/300"}
+				/>
+			</div>
+		</>
 	);
 }
 
