9e682c67f7f86e186cd63d380efc6535ca7e0c57
Walid <ow_bouhenika@esi.dz>
2025-08-17 11:48:28 +0100

    done skills now let's finish this fast

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,6 +1,3 @@
-// src/App.jsx
-/* import "./App.css"; // <--- DELETED THIS LINE*/
-
 import Header from "./components/Header/Header.jsx";
 import Hero from "./components/Hero/Hero.jsx";
 import Aboutme from "./components/About-me/aboutme.jsx";
