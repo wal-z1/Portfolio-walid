bc2d6063d85674b0b76d982364d07b1a9750e2e3
Walid <ow_bouhenika@esi.dz>
2025-08-17 13:39:25 +0100

    timelinecard

---
+++ my-portfolio/src/components/Time-line/timelinecard.jsx
@@ -0,0 +1,27 @@
+function timelinecrad() {
+	return (
+		<div class="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+			<div class="mx-3 mb-0 border-b border-slate-200 pt-3 pb-2 px-1">
+				<span class="text-sm font-medium text-slate-600">
+					MAYBE CHANGE FOR icon
+				</span>
+			</div>
+
+			<div class="p-4">
+				<h5 class="mb-2 text-slate-800 text-xl font-semibold">TITLE</h5>
+				<p class="text-slate-600 leading-normal font-light">
+					DESC TEXT Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Illo, qui, dolorum vel soluta repellendus minima numquam alias impedit
+					neque autem modi, accusantium explicabo! Ad quidem nemo culpa
+					consequuntur maxime commodi.
+				</p>
+			</div>
+			<div class="mx-3 border-t border-slate-200 pb-3 pt-2 px-1">
+				<span class="text-sm text-slate-600 font-medium">
+					Last updated: TIME
+				</span>
+			</div>
+		</div>
+	);
+}
+export default timelinecrad;
