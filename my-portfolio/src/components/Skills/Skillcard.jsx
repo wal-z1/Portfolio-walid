c409514a9a2aa656fa355871dc65a0f86413bacd
Walid <ow_bouhenika@esi.dz>
2025-08-17 10:36:41 +0100

    sure sure

--- my-portfolio/src/components/Skills/Skillcard.jsx
+++ my-portfolio/src/components/Skills/Skillcard.jsx
@@ -1,17 +1,14 @@
 import PropTypes from "prop-types";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 function SkillCard({ title, description, imageUrl }) {
 	return (
 		<div
 			className=" 
-tilt-in-fwd-br
+				tilt-in-fwd-br
         flex items-center rounded-sm shadow-lg 
-       
         bg-[rgba(46,28,70,0.1)] backdrop-blur-sm backdrop-saturate-100
-
         border-1 border-[#441570] 
         shadow-purple-700/20 
-
         transition-colors duration-300 
         hover:border-[hsl(271,70%,64%)]
       ">
@@ -22,12 +19,10 @@ tilt-in-fwd-br
 					alt={`${title} icon`}
 				/>
 			</div>
-
 			<div className="flex flex-col justify-center px-3 leading-normal">
 				<h5 className="mb-1 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
 					{title}
 				</h5>
-
 				<p className="mt-1 text-sm font-normal text-gray-700 dark:text-gray-400">
 					{description}
 				</p>
