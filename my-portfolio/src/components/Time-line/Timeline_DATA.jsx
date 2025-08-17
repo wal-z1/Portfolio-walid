165ea0f28ddbf4c7fdd8b4add72527dc015fbce4
Walid <ow_bouhenika@esi.dz>
2025-08-17 22:01:22 +0100

    Commit data , work on countainer

--- my-portfolio/src/components/Time-line/Timeline_DATA.jsx
+++ my-portfolio/src/components/Time-line/Timeline_DATA.jsx
@@ -1,2 +1,26 @@
-const Timeline_DATA = [{}];
-export default Timeline_DATA;
+const TIMELINE_DATA = [
+	// recent first
+	{
+		category: "Education",
+		title: "Computer Science Engineering Student",
+		text: "Currently pursuing a degree at the Higher National School of Computer Science (ESI), Algiers.",
+		timetext: "2024 - Present",
+		imageUrl: "", // Graduation Cap Icon
+	},
+	{
+		category: "Education",
+		title: "Baccalaureate in Mathematics",
+		text: "Graduated from Nasri Ramdan High School with honors, achieving a score of 18.66/20.",
+		timetext: "July 2024",
+		imageUrl: "",
+	},
+	{
+		category: "Professional Experience",
+		title: "Freelance Video Editor",
+		text: "Edited video content for various clients. Open to new projects.",
+		timetext: "Summer 2022 - Present",
+		imageUrl: "https://img.icons8.com/?size=100&id=398&format=png&color=FAFAFA", // Play Button Icon
+	},
+];
+
+export default TIMELINE_DATA;
--- my-portfolio/src/components/Time-line/Timeline_container.jsx
+++ my-portfolio/src/components/Time-line/Timeline_container.jsx
@@ -3,6 +3,7 @@ import Timelinecrad from "./timelinecard";
 import { motion } from "framer-motion";
 
 function Timeline_container() {
+  Timeline_DATA.map((event)=>)
 	return;
 }
 export default Timeline_container;
