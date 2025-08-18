b03475a9d43b41bbe47c0d2837f0301c2e92bfcc
Walid <ow_bouhenika@esi.dz>
2025-08-18 11:34:51 +0100

    Done , ill make it more comapct thena addd subtle animations

--- my-portfolio/src/components/Time-line/Timeline_container.jsx
+++ my-portfolio/src/components/Time-line/Timeline_container.jsx
@@ -3,15 +3,22 @@ import Timelinecrad from "./timelinecard";
 
 function Timeline_container() {
 	return (
-		<div className=" py-12">
-			{Timeline_DATA.map((event, index) => (
-				<div className="flex items-start ">
+		<div className="py-12">
+			{Timeline_DATA.map((event) => (
+				<div key={event.title} className="flex items-start">
 					<div className="flex flex-col items-center">
-						{/* The Dot */}
-						<div className="h-4 w-4 flex-shrink-0 rounded-full bg-purple-950 "></div>
+						<div
+							className="
+                h-4 w-4 flex-shrink-0 rounded-full m-1 
+                bg-purple-500
+                shadow-[0_0_10px_rgba(168,85,247,0.7)]
+              "></div>
 					</div>
-					{/* The pb-12 adds spacing between each timeline event */}
-					<div className="w-full pb-12 border-l-2 ml-1 pl-10 border-purple-950 ">
+					<div
+						className="
+              w-full pb-4 border-l-2 ml-1 pl-10 border-purple-950 
+              shadow-[-2px_0px_15px_-5px_rgba(168,85,247,0.15)]
+            ">
 						<Timelinecrad
 							title={event.title}
 							category={event.category}
