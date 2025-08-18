87ac718559ae711177b6f96eaf7534862df15dde
Walid <ow_bouhenika@esi.dz>
2025-08-18 11:13:47 +0100

    timeline design done

--- my-portfolio/src/components/Time-line/Timeline_container.jsx
+++ my-portfolio/src/components/Time-line/Timeline_container.jsx
@@ -1,21 +1,29 @@
 import Timeline_DATA from "./Timeline_DATA";
 import Timelinecrad from "./timelinecard";
-import { motion } from "framer-motion";
 
 function Timeline_container() {
 	return (
-		<div>
-			{Timeline_DATA.map((event) => (
-				<Timelinecrad
-					title={event.title}
-					key={event.title}
-					category={event.category}
-					text={event.text}
-					url={event.url}
-					timetext={event.timetext}
-				/>
+		<div className=" py-12">
+			{Timeline_DATA.map((event, index) => (
+				<div className="flex items-start ">
+					<div className="flex flex-col items-center">
+						{/* The Dot */}
+						<div className="h-4 w-4 flex-shrink-0 rounded-full bg-purple-950 "></div>
+					</div>
+					{/* The pb-12 adds spacing between each timeline event */}
+					<div className="w-full pb-12 border-l-2 ml-1 pl-10 border-purple-950 ">
+						<Timelinecrad
+							title={event.title}
+							category={event.category}
+							text={event.text}
+							url={event.url}
+							timetext={event.timetext}
+						/>
+					</div>
+				</div>
 			))}
 		</div>
 	);
 }
+
 export default Timeline_container;
