776969a885c5d3d0c1a4b6ebf510a78dacf4f512
Walid <ow_bouhenika@esi.dz>
2025-08-18 17:28:27 +0100

    done

--- my-portfolio/src/components/Hero/Hero.jsx
+++ my-portfolio/src/components/Hero/Hero.jsx
@@ -22,15 +22,19 @@ function Hero() {
 					</span>
 					<hr className="w-11/12 lg:w-[30rem] m-3 ml-0 bg-white shadow-[0_0_10px_rgba(255,255,255,0.5)] max-[1084px]:self-center" />
 					<div className="button-container flex items-center gap-4 max-[1084px]:self-center">
-						<button className="font-outfit rounded-lg border-2 border-[rgb(33,15,49)] bg-[#501c81] px-6 py-3 text-lg font-medium text-[#FAFAFA] text-shadow-[1px_1px_3px_rgba(0,0,0,0.5)] transition hover:brightness-120 active:brightness-90 cursor-pointer">
+						<a
+							href="#projects"
+							role="button"
+							className="font-outfit rounded-lg border-2 border-[rgb(33,15,49)] bg-[#501c81] px-6 py-3 text-lg font-medium text-[#FAFAFA] text-shadow-[1px_1px_3px_rgba(0,0,0,0.5)] transition hover:brightness-120 active:brightness-90 cursor-pointer no-underline">
 							View My Work
-						</button>
-						<button
-							className="font-outfit rounded-lg border-2 border-[#441570] bg-transparent px-6 py-3 text-lg font-medium text-[#FAFAFA] text-shadow-[1px_1px_3px_rgba(0,0,0,0.5)] transition-colors hover:bg-[#501c81] cursor-pointer
+						</a>
 
-						">
+						<a
+							href="#contact"
+							role="button"
+							className="font-outfit rounded-lg border-2 border-[#441570] bg-transparent px-6 py-3 text-lg font-medium text-[#FAFAFA] text-shadow-[1px_1px_3px_rgba(0,0,0,0.5)] transition-colors hover:bg-[#501c81] cursor-pointer no-underline">
 							Contact Me
-						</button>
+						</a>
 					</div>
 
 					<SocialLinks />
@@ -52,8 +56,8 @@ function Hero() {
 							stiffness: 300,
 							damping: 15,
 						}} /*bounce animation */
-						className="inline-block rounded-[30px] border-2 border-[#331181] shadow-[3px_3px_15px_rgba(73,25,185,0.53)]"
-						src="https://placehold.co/300"
+						className="inline-block rounded-[30px] border-2 border-[#331181] shadow-[3px_3px_15px_rgba(73,25,185,0.53)] w-[300px] h-[300px]  object-contain"
+						src="/Images/pfp.jpg"
 						alt="Person Picture"
 					/>
 				</div>
