2196a6c949fa96a1bf3ea276015a1303bfc9f89e
Walid <ow_bouhenika@esi.dz>
2025-08-30 18:01:38 +0100

    new pj added

--- my-portfolio/src/components/Project/ProjectsData.jsx
+++ my-portfolio/src/components/Project/ProjectsData.jsx
@@ -1,17 +1,22 @@
 export const PROJECTS_DATA = [
 	{
+		title: "Book Tracker",
+		description:
+			"A minimal book tracker simple design, no clutter, just the essentials.",
+		imageUrl: "/Images/booktr.png",
+		liveUrl: "https://github.com/wal-z1/book-tracker",
+	},
+	{
 		title: "1CP Semester Calculator",
 		description:
 			"A web-based calculator to help 1CP students organize and calculate their semester marks.",
 		imageUrl: "/Images/Screenshot 1cpavg .png",
-
 		liveUrl: "https://github.com/wal-z1/1CP-Avg",
 	},
 	{
 		title: "Personal Portfolio",
 		description:
 			"My personal portfolio website to showcase my projects, skills, and professional experience.",
-
 		liveUrl: "https://github.com/wal-z1/Portfolio-walid",
 		imageUrl: "/Images/Screenshot-portfolio.png",
 	},
