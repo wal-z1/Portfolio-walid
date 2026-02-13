a8e6bdb34d63ccb4bb755dc24dd50255efeaf280
Walid Bouhenika <135279492+wal-z1@users.noreply.github.com>
2026-02-13 18:59:26 +0100

    Update Timeline_DATA.jsx

--- my-portfolio/src/components/Time-line/Timeline_DATA.jsx
+++ my-portfolio/src/components/Time-line/Timeline_DATA.jsx
@@ -2,9 +2,16 @@ const TIMELINE_DATA = [
 	// recent first
 	{
 		category: "Achievement",
+		title: "Onsite CTF - Cyber Summit - 2026",
+		text: "Competed with Team IrgaZEN at Cyber Summit hosted by Shellmates & Quanta Club. Placed #2 tied with the 1st team on points... Because Of time difference.
+",
+		timetext: "February 2026",
+	},
+	{
+		category: "Achievement",
 		title: "First On-Site CTF - BSides Algiers 2025",
 		text: "Competed with Team IrgaZEN at BSides Algiers 7th edition hosted by Shellmates. Placed #7 on-site and #18 online out of 330 teams.",
-		timetext: "December 2024",
+		timetext: "December 2025",
 	},
 	{
 		category: "Education",
